036c71ae279ce2f57bf5301695669fdbdb6ff0bb
smithros <kovalr2000@gmail.com>
2021-01-01 15:54:29 +0200

    initial project structure

---
+++ src/main/java/com/kpi/booknet/booknet/BookNetApplication.java
@@ -0,0 +1,13 @@
+package com.kpi.booknet.booknet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpi/booknet/booknet/BookNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.booknet.booknet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
