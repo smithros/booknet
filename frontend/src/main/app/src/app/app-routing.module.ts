045fe1bd4da0b1e3ba0af63d525c680f02a99ab0
smithros <kovalr2000@gmail.com>
2021-02-06 16:30:31 +0200

    Versions downgrade

--- frontend/src/main/app/src/app/app-routing.module.ts
+++ frontend/src/main/app/src/app/app-routing.module.ts
@@ -3,16 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './components/authorization/login/login.component';
 import {UserListComponent} from './components/user-list/user-list.component';
 import {RegisterComponent} from './components/authorization/register/register.component';
-import {ErrorPageComponent} from './components/error-page/error-page.component';
 import {LandingComponent} from './components/landing/landing.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'user/get/all', component: UserListComponent},
-  {path: 'home', component: LandingComponent},
-
-  {path: '**', component: ErrorPageComponent}
+  {path: 'home', component: LandingComponent}
 ];
 
 @NgModule({
