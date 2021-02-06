e5faadd27c80cfd85f38c445435a0093ea5d960d
smithros <kovalr2000@gmail.com>
2021-02-06 20:25:15 +0200

    Added home page

--- frontend/src/main/app/src/app/components/landing/landing.component.ts
+++ frontend/src/main/app/src/app/components/landing/landing.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Book} from '../../models/book';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-landing',
@@ -6,11 +10,22 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  public books: Book[] = [];
+  public url: string = environment.apiBaseUrl;
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.getBooks().toPromise().then(
+      books => {
+        this.books = books;
+      }
+    );
   }
 
   ngOnInit(): void {
   }
 
+  public getBooks(): Observable<Book[]> {
+    const url = `${this.url}/book/all`;
+    return this.http.get<Book[]>(url);
+  }
 }
