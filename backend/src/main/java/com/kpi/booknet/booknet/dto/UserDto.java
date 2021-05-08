f2bf91bd47f1d1fd74d9ce234e94fdce4e585570
smithros <kovalr2000@gmail.com>
2021-05-08 16:01:20 +0300

    Removed not needed models

--- backend/src/main/java/com/kpi/booknet/booknet/dto/UserDto.java
+++ backend/src/main/java/com/kpi/booknet/booknet/dto/UserDto.java
@@ -44,5 +44,4 @@ public final class UserDto implements Serializable {
     private String recoverCode;
     private boolean verified;
     private boolean activated;
-    private Set<AchievementDto> achievements;
 }
